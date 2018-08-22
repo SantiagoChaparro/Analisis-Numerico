fb91268f561f4e83ae1cd72e5f2237b7b1f13b3d
SantiagoChaparro <41376467+SantiagoChaparro@users.noreply.github.com>
2018-08-22 08:58:43 -0500

    Add files via upload

--- Parcial/punto 2.R
+++ Parcial/punto 2.R
@@ -1,12 +1,19 @@
 fDeX <- function(x)
 {
-  return(log(x+2))
+  return(log(x+2)-sin(x))
 }
 
 
 funcionRecursiva<- function(n,vectorSolucion)
 {
-  
+  if(fDeX(vectorSolucion[1]<=0.00000001))
+  {
+    return (vectorSolucion[1])
+  }
+  else
+  {
+    return (vectorSolucion[2]-(fDeX(vectorSolucion[2])*(vectorSolucion[1]-vectorSolucion[2])/fDeX(vectorSolucion[1])))
+  }
   
 }
 
